a56c44cb30e30348d3193ae8dcb46cc914ed03e9
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-11-12 22:04:40 +0530

    Basic

--- intro.py
+++ intro.py
@@ -1,3 +1,8 @@
 print("hello\'s Laptop ")
 print('Hello\'s "Laptop"')
 print("Hello's \"Laptop\"")  # escape character.
+
+
+name='himanshu'
+print(name[0:2]) #end is not gonna print so output is hi
+print(name[:5]) 
