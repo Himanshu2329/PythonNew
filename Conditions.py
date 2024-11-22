84d4920a8fd62fb3fcadc80696c3de3b843446a7
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-11-22 23:20:54 +0530

    Conditions

---
+++ Conditions.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age\n"))
+if age==18:
+    print("You are an adult")
+elif age==19:
+    print("bhg ja")
+# elif age>19 | age <21:  #logical error 
+elif age>19 and age<21:
+    print("best h bhai")    
+else:
+    print("chla ja")        
