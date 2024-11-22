6002ace3f614bbed08411824bde0849cff39d805
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-11-22 23:45:11 +0530

    Loops

--- Conditions.py
+++ Conditions.py
@@ -8,3 +8,15 @@ elif age>19 and age<21:
     print("best h bhai")    
 else:
     print("chla ja")        
+
+# Loops
+
+# for i in range(10):  # This loop will execute n-1 times for this case 0 to 9
+#     print(i)
+
+# for i in range(2,20):
+#     print(i+1)  # This loop will execute n-1 times for this case 2 to 19
+
+for i in range(2,20,2):
+    print(i) # this loop will start from 2 to 20-1 and increment by 2, so output will be even numbers
+    
